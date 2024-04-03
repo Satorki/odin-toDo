995d48536f996484b9fa7d949a20961360db825b
satork <sebastian.kedzier@gmail.com>
2024-04-03 21:10:30 +0200

    choseList and descriptionShow functions added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 //LIST CREATE
 class List {
   taskList = [];
-  constructor(title, description) {
+  constructor(id, title, description) {
+    this.id = id;
     this.title = title;
     this.description = description;
   }
@@ -12,7 +13,6 @@ class List {
     return this.taskList;
   }
 }
-
 //TASK CREATE
 class Task {
   constructor(id, name, description, date, priority) {
@@ -24,26 +24,23 @@ class Task {
   }
 }
 
-class TaskFactory {
-  id = 1;
-  #generateId() {
-    return this.id++;
+//LIST AND TASK WITH ID
+function IdGenerator() {
+  let id = 1;
+  function generateId() {
+    return id++;
   }
-  create(name, description, date, priority) {
-    return new Task(this.#generateId(), name, description, date, priority);
+
+  function listWithId(title, description) {
+    return new List(generateId(), title, description);
   }
+
+  function taskWithId(name, description, date, priority) {
+    return new Task(generateId(), name, description, date, priority);
+  }
+
+  return { listWithId, taskWithId };
 }
-
-//TESTS
-// const today = new List("Today", "to do today");
-
-// const taskFactory = new TaskFactory();
-
-// const doAMathForSchool = taskFactory.create("do zupa", "zupa good");
-
-// today.addTask(doAMathForSchool);
-
-// console.log(today.showTasks());
 
 //MODAL SHOW
 function ModalList() {
@@ -70,75 +67,124 @@ const modal = ModalList();
 modal.showModal();
 modal.closeModal();
 
+const ids = IdGenerator();
+
 //LIST SHOW
-function ListAdd() {
+function ListBehaviours() {
   const selectors = {
     addListBtn: document.querySelector("#addListBtn"),
     listField: document.querySelector("#listField ul"),
     listName: document.querySelector("#listName"),
     listDescription: document.querySelector("#listDescriptionArea"),
     backBtn: document.querySelector("#backBtn"),
+    listTitleMain: document.querySelector("#listTitleMain"),
   };
   let completedList = [];
 
   function createList() {
-    return new List(selectors.listName.value, selectors.listDescription.value);
+    return ids.listWithId(
+      selectors.listName.value,
+      selectors.listDescription.value
+    );
   }
   function saveList() {
     completedList.push(createList());
     return completedList;
+  }
+  function generateList() {
+    selectors.listField.textContent = "";
+    completedList.forEach((element) => {
+      const listElementsAdd = {
+        li1: document.createElement("li"),
+        li2: document.createElement("li"),
+        span1: document.createElement("span"),
+        span2: document.createElement("span"),
+        span3: document.createElement("span"),
+      };
+      listElementsAdd.span1.textContent = "✗";
+      listElementsAdd.span1.classList.add("deleteList");
+      listElementsAdd.span2.textContent = element.title;
+      listElementsAdd.span2.classList.add("spanLink");
+      listElementsAdd.span3.textContent = " ...";
+      listElementsAdd.span3.setAttribute("id", "listDescriptionBtn");
+      listElementsAdd.span3.classList.add("spanLink");
+      listElementsAdd.li2.textContent = element.description;
+      listElementsAdd.li2.setAttribute("id", "listDescription");
+      listElementsAdd.li2.classList.add("listDescription");
+
+      selectors.listField.appendChild(listElementsAdd.li1);
+      listElementsAdd.li1.appendChild(listElementsAdd.span1);
+      listElementsAdd.li1.appendChild(listElementsAdd.span2);
+      listElementsAdd.li1.appendChild(listElementsAdd.span3);
+      selectors.listField.appendChild(listElementsAdd.li2);
+    });
   }
   function addList() {
     selectors.addListBtn.addEventListener("click", () => {
       saveList();
     });
   }
-  function generateList() {
-    selectors.listField.textContent = "";
-    completedList.forEach((element, index) => {
-      const listElementsAdd = {
-        li1: document.createElement("li"),
-        li2: document.createElement("li"),
-        span1: document.createElement("span"),
-        span2: document.createElement("span"),
-      };
-
-      listElementsAdd.span1.textContent = element.title;
-      listElementsAdd.span2.textContent = " ...";
-      listElementsAdd.span2.setAttribute("id", "listDescriptionBtn" + index);
-      listElementsAdd.span2.classList.add("spanLink");
-      listElementsAdd.li2.textContent = element.description;
-      listElementsAdd.li2.setAttribute("id", "listDescription" + index);
-      listElementsAdd.li2.classList.add("listDescription");
-
-      selectors.listField.appendChild(listElementsAdd.li1);
-      listElementsAdd.li1.appendChild(listElementsAdd.span1);
-      listElementsAdd.li1.appendChild(listElementsAdd.span2);
-      selectors.listField.appendChild(listElementsAdd.li2);
-    });
-  }
   function showList() {
     selectors.backBtn.addEventListener("click", () => {
       generateList();
+      descriptionShow();
+    });
+  }
+  // function deleteList() {
+  //   document.querySelectorAll(".deleteList").forEach((element, index) => {
+  //     element.addEventListener("click", () => {
+  //       completedList.splice(index, 1);
+  //       console.table(completedList);
+  //       generateList();
+  //     });
+  //   });
+  // }
+  const titleH2 = document.createElement("h2");
+  function chooseList() {
+    document.querySelectorAll("#listTitle").forEach((element) => {
+      element.addEventListener("click", () => {
+        console.log(element.textContent);
+        selectors.listTitleMain.appendChild(titleH2);
+        titleH2.textContent = element.textContent;
+      });
     });
   }
 
-  return { createList, addList, showList };
+  return { createList, addList, showList, completedList, chooseList };
 }
-const mainList = ListAdd();
+
+const mainList = ListBehaviours();
 mainList.createList();
 mainList.addList();
 mainList.showList();
+mainList.chooseList();
+
+// mainList.deleteList();
 
 // DESCRIPTION SHOW
-function DescriptionShow() {
-  const selectors = {
-    btn: document.querySelector("#listDescriptionBtn0"),
-    description: document.querySelector("#listDescription0"),
-  };
-  selectors.btn.addEventListener("click", () => {
-    selectors.description.style.display = "none"
+function descriptionShow() {
+  document.querySelectorAll("#listDescriptionBtn").forEach((element) => {
+    element.addEventListener("click", () => {
+      if (element.closest("li").nextElementSibling.style.display === "block") {
+        element.closest("li").nextElementSibling.style.display = "none";
+      } else {
+        element.closest("li").nextElementSibling.style.display = "block";
+      }
+    });
   });
 }
 
-DescriptionShow();
+let temp = 1;
+document.querySelector("#deleteListBtn").addEventListener("click", () => {
+  if (temp === 0) {
+    document.querySelectorAll(".deleteList").forEach((element) => {
+      element.style.display = "none";
+    });
+    temp = 1;
+  } else {
+    document.querySelectorAll(".deleteList").forEach((element) => {
+      element.style.display = "block";
+    });
+    temp = 0;
+  }
+});
